7370d6daa1bccb0c1ee7a616d283ee92ba01e000
Apermar87 <ajperezmarin@gmail.com>
2024-11-07 17:22:26 +0100

    Ajustar estilos en todas las secciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
@@ -12,4 +12,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portafolio';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0); // Esto asegura que el scroll regrese al tope
+      }
+    });
+  }
 }
