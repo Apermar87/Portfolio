76fa14fe220adc7e2908d94888e667bab60df08d
Apermar87 <ajperezmarin@gmail.com>
2024-11-13 18:26:39 +0100

    Responsive al dia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
@@ -8,9 +8,9 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corregido 'styleUrl' a 'styleUrls'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portafolio';
 
   constructor(private router: Router) {
@@ -18,6 +18,34 @@ export class AppComponent {
       if (event instanceof NavigationEnd) {
         window.scrollTo(0, 0); // Esto asegura que el scroll regrese al tope
       }
+    });
+  }
+
+  ngOnInit() {
+    // Obtener el elemento del menú toggle y el contenido
+    const menuToggle = document.getElementById('menu-toggle') as HTMLInputElement;
+    const mainContent = document.getElementById('main-content') as HTMLElement;
+
+    // Verificar si el menú toggle existe y manejar el cambio de estado
+    if (menuToggle && mainContent) {
+      menuToggle.addEventListener('change', () => {
+        if (menuToggle.checked) {
+          mainContent.style.marginTop = '400px'; // Ajusta el valor según la altura de tu menú desplegable
+        } else {
+          mainContent.style.marginTop = '0';
+        }
+      });
+    }
+
+    // Cerrar el menú al hacer clic en un enlace de navegación
+    const navLinks = document.querySelectorAll('.nav-links a');
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        if (menuToggle) {
+          menuToggle.checked = false; // Desmarcar el checkbox para cerrar el menú
+          mainContent.style.marginTop = '0'; // Opcional: restablecer el margen superior
+        }
+      });
     });
   }
 }
