ede9e2dfdbb9aa9373d20a64c173382ee2602a20
Apermar87 <ajperezmarin@gmail.com>
2024-11-14 18:57:07 +0100

    Empiezo contacto

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
+
+interface ContactForm {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  form: ContactForm = {
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  };
 
+  send() {
+    console.log(this.form);
+
+    emailjs
+      .send(
+        'service_fldid35',
+        'template_88mq6y1',
+        { ...this.form },
+        {
+          publicKey: 'VSQZm6xzmwWJQJMT-'
+        }
+      ).then(() => {
+        console.log('Sent!')
+      })
+  }
 }
