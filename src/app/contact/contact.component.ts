85d7d9badedcf3f4fc56b6fd7ec8fed4606f4d06
Apermar87 <ajperezmarin@gmail.com>
2024-11-18 16:50:54 +0100

    problemas emailjs

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import emailjs from '@emailjs/browser';
 
@@ -24,19 +25,19 @@ export class ContactComponent {
     message: '',
   };
 
+  http = inject(HttpClient)
+
   send() {
     console.log(this.form);
 
-    emailjs
-      .send(
-        'service_fldid35',
-        'template_88mq6y1',
-        { ...this.form },
-        {
-          publicKey: 'VSQZm6xzmwWJQJMT-'
-        }
-      ).then(() => {
-        console.log('Sent!')
-      })
+    this.http.post('https://api.emailjs.com/api/v1.0/email/send', {
+      lib_version: "4.4.1",
+      service_id: "service_fldid35",
+      template_id: "template_88mq6y1",
+      template_params: this.form,
+      user_id: "VSQZm6xzmwWJQJMT-",
+    }).subscribe(() => {
+      console.log('sent!');
+    })
   }
 }
